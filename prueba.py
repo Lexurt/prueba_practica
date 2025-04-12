ac3987f6c64daee254971be7cf60cc5868a04396
Lexurt <orcajada135@gmail.com>
2025-04-12 12:31:20 +0200

    Update prueba.py
    cambios necesarios

--- prueba.py
+++ prueba.py
@@ -1,5 +1,7 @@
 edad=(int(input("Escribe tu edad")))
- if edad =< 18:
+if edad <= 18:
 	print("Eres mayor de edad")
- else:
+elif edad == 20:
+	print("tienes 20 años")
+else:
 	print("Eres menor de edad")
